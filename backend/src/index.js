9dd96337177e96e54d12567599106a579f50d5d1
rubens-lavor <haryson07@gmail.com>
2020-08-15 21:11:30 -0300

    salvando imagens no banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require('mongoose');
 
 const app = express();
+
+/**
+ * Database setup
+ */
+
+mongoose.connect('mongodb://localhost:27017/upload',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,19 @@ const routes = require("express").Router();
 const multer = require("multer"); //permite realizar uploads
 const multerConfig = require("./config/multer");
 
-routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
-    console.log(req.file);
+const Post = require("./models/Post");
 
-    return res.json({ hello:"world" });
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+    //console.log(req.file);
+
+    const post = await Post.create({
+        name: req.file.originalname,
+        size: req.file.size,
+        key: req.file.filename,
+        url:'',
+    });
+
+    return res.json(post);
 });
 
 module.exports = routes;
